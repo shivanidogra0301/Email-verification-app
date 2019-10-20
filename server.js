c9a96e274bd579420a9dffab89f6177dca8b23bf
Shubham <shubhamdogra112@gmail.com>
2019-10-20 09:45:39 +0530

    all file added

---
+++ server.js
@@ -0,0 +1,38 @@
+var express=require("express"),
+    request=require("request");
+    
+
+ var app=express();
+ app.set("view engine","ejs");
+ 
+
+ 
+
+ app.get("/",function(req,res){
+
+         res.render("search");
+         
+      })
+
+ 
+
+ app.get("/results",function(req,res){
+   var email=req.query.Email;
+
+   var url="https://api.trumail.io/v2/lookups/json?email="+email
+   request(url,function(err,response,body){
+      if(!err&&response.statusCode==200){
+         
+         res.render("result",{data:body})
+      }
+
+
+ })
+});
+ 
+
+ app.listen(3000,function(){
+
+    console.log("Server has started");
+
+ })
